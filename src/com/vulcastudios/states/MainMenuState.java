bae9c467679a4a3c5af68eac1c68a9d76f09b252
Stephanie Reese <reese.sm@gmail.com>
2012-04-07 16:44:37 -0400

    Merge

--- src/com/vulcastudios/states/MainMenuState.java
+++ src/com/vulcastudios/states/MainMenuState.java
@@ -1,5 +1,8 @@
 package com.vulcastudios.states;
 
+import java.util.ArrayList;
+
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -9,19 +12,30 @@ import org.newdawn.slick.state.StateBasedGame;
 import com.vulcastudios.TestGame;
 
 public class MainMenuState extends BasicGameState {
+	
+	private ArrayList<String> menuStrings = new ArrayList<String>();
+	private int selection;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		// TODO Auto-generated method stub
-
+		menuStrings.add("play");
+		menuStrings.add("controls");
+		menuStrings.add("options");
+		menuStrings.add("credits");
+		
+		selection = 0;
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-
+		g.setColor(Color.green);
+		g.drawString("play", 200, 200);
+		g.setColor(Color.white);
+		g.drawString("options", 550, 200);
+		g.drawString("controls", 200, 400);
+		g.drawString("credits", 550, 400);
 	}
 
 	@Override
@@ -36,4 +50,7 @@ public class MainMenuState extends BasicGameState {
 		return TestGame.MAIN_MENU_STATE_ID;
 	}
 
+	public void drawMenu(Graphics g) {
+		
+	}
 }
