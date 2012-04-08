3efdab16ff8f3365d123c12cacc4c2642d3e010b
Andrew Melton <ramielrowe@gmail.com>
2012-04-08 14:41:07 -0400

    Including New State Backgrounds

--- src/com/vulcastudios/states/ControlsState.java
+++ src/com/vulcastudios/states/ControlsState.java
@@ -30,7 +30,7 @@ public class ControlsState extends BasicGameState {
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		Color previousColor = g.getColor();
-		g.drawImage(this.rm.getImage("creditsScreen"), 0, 0);
+		g.drawImage(this.rm.getImage("controlsScreen"), 0, 0);
 		g.drawString("Directional arrows: move", 50, 50);
 		g.drawString("Space bar: suicide", 50, 75);
 		g.drawString("Enter: restart level", 50, 100);
