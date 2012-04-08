d1b12685a1008c7b6b523497fd662ae14e353aaf
Stephanie Reese <reese.sm@gmail.com>
2012-04-08 00:41:51 -0400

    esc goes to the main menu

--- src/com/vulcastudios/states/TransitionState.java
+++ src/com/vulcastudios/states/TransitionState.java
@@ -2,6 +2,7 @@ package com.vulcastudios.states;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -23,18 +24,19 @@ public class TransitionState extends BasicGameState {
 		long finalTime = ((TestGame)game).getCurrentLevel().getFinalTime();
 		double seconds = finalTime / 1000.0;
 		g.drawString("Final Time: " + seconds + " seconds", 50, 50);
+		g.drawString("Press Enter to continue to the next level", 50, 100);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-
+		if (container.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
+			game.enterState(TestGame.MAIN_MENU_STATE_ID);
+		}
 	}
 
 	@Override
 	public int getID() {
-		// TODO Auto-generated method stub
 		return TestGame.TRANSITION_STATE;
 	}
 
